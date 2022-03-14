3c1658287341f5ed945a15a31357377b7b51ee33
Aman Agrawal <aman9893089064@gmail.com>
2022-03-14 10:25:37 +0530

    Binary Serilization tried

--- Program.cs
+++ Program.cs
@@ -1,77 +1,34 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Delegates
 {
-    class Program
+    [Serializable]
+    class Student
     {
-        public static void Main(string[] args)
+        public int rollNo;
+        public string name;
+        public Student(int rollNo, string name)
         {
-            DirectoryInfo dir = new DirectoryInfo("P:\\PersistentSystems\\Demo\\Directory");
-            try
-            {
-                if (dir.Exists)
-                {
-                    Console.WriteLine("Bani to hai");
-                }
-                dir.Create();
-                Console.WriteLine("Bana liye");
-                dir.Delete();
-                Console.WriteLine("Mita diya xD");
-            }
-
-            catch(Exception ex)
-            {
-                Console.WriteLine("Nahi mili yaar" + ex.ToString());
-            }
-            
-            Console.Read();
+            this.rollNo = rollNo;
+            this.name = name;
         }
-        
-        //public static void Main(string[] args)
-        //{
-            //WriteText();
-            //ReadText();
-            //string text = "Hello kon bol raha hai" + "Hum bol rahe hai" + "Hum Kon" + " phone pakdhao unko";
-            //StringBuilder sb = new();
-            //StringWriter sw = new(sb);
-            //sw.WriteLine(text);
-            //sw.Close();
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("P:\\PersistentSystems\\Demo\\Serilization.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bn = new BinaryFormatter();
+            Student s = new Student(101, "Xlib");
+            bn.Serialize(fs, s);
+            fs.Close();
+            Console.Read();
 
-            //StringReader sr = new(sb.ToString());
-            //while(sr.Peek () > -1)
-            //{
-            //    Console.WriteLine(sr.ReadLine());
-            //}
-
-            //Console.Read();
-        //}
-
-        //public static void WriteText()
-        //{
-        //    using (BinaryWriter bw = new BinaryWriter(File.Open("P:\\PersistentSystems\\Demo\\BinaryWriter.txt", FileMode.CreateNew)))
-        //    {
-        //        bw.Write(12.5);
-        //        bw.Write("This is the string");
-        //        bw.Write(true);
-        //    }
-        //    Console.WriteLine("Can't write more xD");
-        //}
-
-        //public static void ReadText()
-        //{
-        //    Console.WriteLine();
-        //    Console.WriteLine("Now Let's Read what was written");
-        //    using (BinaryReader br = new BinaryReader(File.Open("P:\\PersistentSystems\\Demo\\BinaryWriter.txt", FileMode.Open)))
-        //    {
-        //        Console.WriteLine(br.ReadDouble());
-        //        Console.WriteLine(br.ReadString());
-        //        Console.WriteLine(br.ReadBoolean());
-        //    }
-        //    Console.WriteLine("That's All I can Read");
-        //}
-
+        }
     }
 }
 
@@ -95,7 +52,77 @@ namespace Delegates
 
 
 
+//class Program
+//{
 
+
+//public static void Main(string[] args)
+//{
+//    DirectoryInfo dir = new DirectoryInfo("P:\\PersistentSystems\\Demo\\Directory");
+//    try
+//    {
+//        if (dir.Exists)
+//        {
+//            Console.WriteLine("Bani to hai");
+//        }
+//        dir.Create();
+//        Console.WriteLine("Bana liye");
+//        dir.Delete();
+//        Console.WriteLine("Mita diya xD");
+//    }
+
+//    catch(Exception ex)
+//    {
+//        Console.WriteLine("Nahi mili yaar" + ex.ToString());
+//    }
+
+//    Console.Read();
+//}
+
+//public static void Main(string[] args)
+//{
+//WriteText();
+//ReadText();
+//string text = "Hello kon bol raha hai" + "Hum bol rahe hai" + "Hum Kon" + " phone pakdhao unko";
+//StringBuilder sb = new();
+//StringWriter sw = new(sb);
+//sw.WriteLine(text);
+//sw.Close();
+
+//StringReader sr = new(sb.ToString());
+//while(sr.Peek () > -1)
+//{
+//    Console.WriteLine(sr.ReadLine());
+//}
+
+//Console.Read();
+//}
+
+//public static void WriteText()
+//{
+//    using (BinaryWriter bw = new BinaryWriter(File.Open("P:\\PersistentSystems\\Demo\\BinaryWriter.txt", FileMode.CreateNew)))
+//    {
+//        bw.Write(12.5);
+//        bw.Write("This is the string");
+//        bw.Write(true);
+//    }
+//    Console.WriteLine("Can't write more xD");
+//}
+
+//public static void ReadText()
+//{
+//    Console.WriteLine();
+//    Console.WriteLine("Now Let's Read what was written");
+//    using (BinaryReader br = new BinaryReader(File.Open("P:\\PersistentSystems\\Demo\\BinaryWriter.txt", FileMode.Open)))
+//    {
+//        Console.WriteLine(br.ReadDouble());
+//        Console.WriteLine(br.ReadString());
+//        Console.WriteLine(br.ReadBoolean());
+//    }
+//    Console.WriteLine("That's All I can Read");
+//}
+
+//}
 
 
 
